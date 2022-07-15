77e12938e5e744279cadcd90bc0efdfbff7cd6ff
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-07-15 19:27:40 +0300

    Made ResultsVC for the second screen and IBOutlets for labels and IBAction for recalculate button

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by ALEKSEY SUSLOV on 15.07.2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+}
